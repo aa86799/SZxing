f38fe2575ae9b0283901c83d42a8549683ab99a1
stone <aa86799@163.com>
2018-06-20 11:16:26 +0800

    集成 google-zxing 自带的安卓示例工程

---
+++ app/src/main/java/com/stone/szxing/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.stone.szxing
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * desc  :
+ * author: stone
+ * email : shizongyin@meishubao.com
+ * time  : 24/10/2017 18 51
+ */
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
